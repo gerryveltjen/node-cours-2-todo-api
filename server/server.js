91d976d0d29462623b14635461cbb43e49244900
gerry veltjen <gerry.veltjen@hotmail.be>
2018-09-03 23:36:32 +0200

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -37,11 +37,25 @@ app.get('/todos/:id', (req, res) => {
         return res.status(404).send();
     }
     Todo.findById(id).then((todo) => {
-        if(todo) {
-            res.send({todo});
-        } else {
+        if(!todo) {
             return res.status(404).send();
         }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
     }).catch((e) => {
         res.status(400).send();
     });
