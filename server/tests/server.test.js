8b6b62095af5466244e45f16d746a2879951f6ab
gerry veltjen <gerry.veltjen@hotmail.be>
2018-09-09 13:35:17 +0200

    Add test for DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -212,7 +212,7 @@ describe('POST /users', () => {
                     expect(user).toExist();
                     expect(user.password).toNotBe(password);
                     done();
-                });
+                }).catch((e) => done(e));
             });
     });
 
@@ -283,6 +283,25 @@ describe('POST /users/login', () => {
                 }
 
                 User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
                     expect(user.tokens.length).toBe(0);
                     done();
                 }).catch((e) => done(e));
