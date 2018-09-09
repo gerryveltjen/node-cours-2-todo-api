aea368b41ca5e929588084380133d28b800f2f94
gerry veltjen <gerry.veltjen@hotmail.be>
2018-09-09 12:43:05 +0200

    Add tests for POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -71,6 +71,25 @@ UserSchema.statics.findByToken = function (token) {
        'tokens.access': 'auth'
     });
 };
+UserSchema.statics.findByCredentials = function (email, password) {
+    const user = this;
+
+    return User.findOne({email}).then((user) => {
+       if(!user) {
+           return Promise.reject();
+       }
+
+       return new Promise((resolve, reject) => {
+           bcrypt.compare(password, user.password, (err, res) => {
+               if(res) {
+                   resolve(user);
+               } else {
+                   reject();
+               }
+           });
+       });
+    });
+};
 
 UserSchema.pre('save', function(next) {
     const user = this;
